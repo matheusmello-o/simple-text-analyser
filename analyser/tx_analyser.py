10a7b524c11b7059fed2f63b385678d9ea99fc1e
matheusmello-o <31773902+matheusmello-o@users.noreply.github.com>
2024-01-06 00:36:06 -0300

    Adding new class App to create the window with the results and added some modifications in Text_Analyser class

--- analyser/tx_analyser.py
+++ analyser/tx_analyser.py
@@ -1,6 +1,30 @@
-class Text_Analyser():
-    def __init__(self, text = None):
-        self.text = text
+import tkinter as tk
+
+class App(tk.Frame):
+    def __init__(self, master = None):
+        super().__init__(master)
+        self.master = master
+        self.text_analyser = Text_Analyser()
+        self.window_creation()
+
+    def text_capture(self):
+        self.text_entry = tk.Entry(self)
+        self.text_entry.pack(side = 'left')
+        
+    def window_creation(self):
+        self.text_capture()
+
+        self.button_statistics_calculation = tk.Button(self,
+                                                       text = 'Statistics Calculation',
+                                                       command = self.text_analyser.statistics_calculation)
+        self.button_statistics_calculation.pack(side = 'left')
+
+        self.label_results = tk.Label(self, text='')
+        self.label_results.pack(side="bottom")
+
+class Text_Analyser(App):
+    def __init__(self):
+        self.text = App.text_capture()
 
     def count_words(self):
         return len(self.text.split())
@@ -33,3 +57,15 @@ class Text_Analyser():
             words_rank = dict(sorted(words_rank.items(), key=lambda item: item[1], reverse = True))
             top5 = dict(islice(words_rank.items(), 5))
             return top5
+    
+    def statistics_calculation(self):
+        count_words = self.count_words()
+        count_letters = self.count_letters()
+        top_words = self.top_most_frequent_words()
+
+        string_results = f'''
+            count of words: {count_words}
+            count of letters: {count_letters}
+            top 5 words: {top_words}
+        '''
+        self.label_results.config(text = string_results) # label created in App class
