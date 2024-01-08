09d13aac41b623faf282393ae6f88fa116e67e1e
matheusmello-o <31773902+matheusmello-o@users.noreply.github.com>
2024-01-07 22:16:27 -0300

    Refactoring classes, methods and organizing code in general

--- analyser/tx_analyser.py
+++ analyser/tx_analyser.py
@@ -1,71 +1,78 @@
 import tkinter as tk
 
-class App(tk.Frame):
+class App:
     def __init__(self, master = None):
-        super().__init__(master)
         self.master = master
-        self.text_analyser = Text_Analyser()
         self.window_creation()
-
-    def text_capture(self):
-        self.text_entry = tk.Entry(self)
-        self.text_entry.pack(side = 'left')
         
     def window_creation(self):
-        self.text_capture()
+        self.text_entry_widget = tk.Entry(self.master)
+        self.text_entry_widget.pack(side = 'left')
 
-        self.button_statistics_calculation = tk.Button(self,
+        self.button_statistics_calculation = tk.Button(self.master,
                                                        text = 'Statistics Calculation',
-                                                       command = self.text_analyser.statistics_calculation)
+                                                       command = self.statistics_calculation)
         self.button_statistics_calculation.pack(side = 'left')
 
-        self.label_results = tk.Label(self, text='')
+        self.label_results = tk.Label(self.master, text='')
         self.label_results.pack(side="bottom")
+    
+    def text_capture(self):
+        return self.text_entry_widget.get() # catching text that were create in window_creation() methoded
+        
+    def statistics_calculation(self):
+        text = self.text_capture()
+        text_analyser = Text_Analyser()
+        
+        text_analysed = text_analyser.analyse_text(text)
+        
+        self.label_results.config(text = text_analysed)
 
-class Text_Analyser(App):
+class Text_Analyser:
     def __init__(self):
-        self.text = App.text_capture()
+        pass
 
-    def count_words(self):
-        return len(self.text.split())
+    def count_words(self, text):
+        return len(text.split())
 
-    def count_letters(self):
-        return len(self.text.replace(' ', ''))
+    def count_letters(self, text):
+        return len(text.replace(' ', ''))
 
-    def count_blank_space(self):
-        return self.text.count(' ')
+    def count_blank_space(self, text):
+        return text.count(' ')
     
-    def words(self):
-        return self.text.split()
+    def words(self, text):
+        return text.split()
     
-    def mean_letters_per_words(self):
-        mean = self.count_letters() / self.count_words()
+    def mean_letters_per_words(self, text):
+        mean = self.count_letters(text) / self.count_words(text)
         return mean
     
-    def top_most_frequent_words(self, only_more_than_3_letters = False):
+    def top_most_frequent_words(self, text, only_more_than_3_letters = False):
         from collections import Counter
         from itertools import islice
 
         if only_more_than_3_letters is True:
-            filtered_words_more_3_letters = [word for word in self.words() if len(word) > 3]
+            filtered_words_more_3_letters = [word for word in self.words(text) if len(word) > 3]
             words_rank = Counter(filtered_words_more_3_letters)
             words_rank = dict(sorted(words_rank.items(), key=lambda item: item[1], reverse = True))
             top5 = dict(islice(words_rank.items(), 5))
             return top5
         else:
-            words_rank = Counter(self.words())
+            words_rank = Counter(self.words(text))
             words_rank = dict(sorted(words_rank.items(), key=lambda item: item[1], reverse = True))
             top5 = dict(islice(words_rank.items(), 5))
             return top5
     
-    def statistics_calculation(self):
-        count_words = self.count_words()
-        count_letters = self.count_letters()
-        top_words = self.top_most_frequent_words()
+    def analyse_text(self, text):
+        count_words = self.count_words(text)
+        count_letters = self.count_letters(text)
+        top_words = self.top_most_frequent_words(text)
 
         string_results = f'''
             count of words: {count_words}
             count of letters: {count_letters}
             top 5 words: {top_words}
         '''
-        self.label_results.config(text = string_results) # label created in App class
+
+        return string_results
--- src.py
+++ src.py
@@ -1,25 +1,10 @@
-#import sys
-#sys.path.append('analyser/')
-from analyser.tx_analyser import Text_Analyser
+from analyser.tx_analyser import App
 import tkinter as tk 
 
-def creating_window():
-    window = tk.Tk()
-    window.title('Text Analyser')
-    window.geometry('600x400')
-    
-    text_box = tk.Entry(window)
-    text_box.pack(pady = 50)
-
-    window.mainloop()
-
-
 def main():
-    text = 'testing the most pica das galaxias'
-    analyser = Text_Analyser(text)
-    
-    creating_window()
-
+    root = tk.Tk()
+    App(master=root)
+    root.mainloop()
 
 if __name__ == '__main__':
     main()
